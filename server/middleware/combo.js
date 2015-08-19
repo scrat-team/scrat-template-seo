7142dff71e93dca9f81d0687f2a34b971cac507e
张云龙 <fouber.NET@gmail.com>
2015-08-19 18:41:59 +0800

    修复combo请求如果缺少某些文件会正常响应但错误内容会被缓存的bug

--- server/middleware/combo.js
+++ server/middleware/combo.js
@@ -15,6 +15,12 @@ module.exports = function (options, app, PROD) {
         logger = app.get('logger') || console,
         lastHash, cached = {};
     return function (req, res) {
+
+        if(PROD && req.headers['if-modified-since']){
+            res.status(304).end();
+            return
+        }
+
         var i = req.originalUrl.indexOf('??'),
             j = req.originalUrl.indexOf('&'),
             url, ext, hash, files, contents = [], rs;
@@ -27,7 +33,12 @@ module.exports = function (options, app, PROD) {
                 lastHash = hash;
                 cached = {};
             }
-            res.setHeader('Cache-Control', 'public, max-age=' + (PROD ? 60 * 60 * 24 * 365 : 0));
+            if(PROD){
+                res.setHeader('Expires', 'Mon, 1 Jan 2100 00:00:00 GMT')
+                res.setHeader('Last-Modified', 'Mon, 1 Jan 2100 00:00:00 GMT')
+                res.setHeader('Cache-Control', 'public, max-age=' + 60 * 60 * 24 * 365);
+                res.setHeader('Pragma', 'public');
+            }
             files = url.split(',');
             files.forEach(function (file) {
                 if(useCache && cached.hasOwnProperty(file)){
@@ -47,11 +58,11 @@ module.exports = function (options, app, PROD) {
                     }
                 }
             });
-            rs = contents.join('\n');
             if (contents.length !== files.length) {
-                logger.error('[combo] some files not found');
+                res.status(404).end();
+            } else {
+                res.send(contents.join('\n'));
             }
-            res.send(rs);
         } else {
             res.send('I am a combo service :)');
         }
